0949ed309ad21b122167e3d6d0f463d5bdebe000
ndescalzi <ndescalzi@gmail.com>
2021-06-28 15:22:28 -0300

    Actualizacion

---
+++ main.py
@@ -0,0 +1,39 @@
+import unittest
+import funciones
+
+
+def impresion(error):
+    print("Los errores son: ")
+    for i in error:
+        print(i + ". ")
+
+
+class Api_Estructura(unittest.TestCase):
+
+    def test_para_nasa(self):
+        resultado = funciones.llaves("nasa")
+        rta = True
+        if resultado:
+            rta = False
+            impresion(resultado)
+        self.assertTrue(rta)
+
+    def test_para_covid(self):
+        resultado = funciones.llaves("covid")
+        rta = True
+        if resultado:
+            rta = False
+            impresion(resultado)
+        self.assertTrue(rta)
+
+    def test_para_finanzas(self):
+        resultado = funciones.llaves("finanzas")
+        rta = True
+        if resultado:
+            rta = False
+            impresion(resultado)
+        self.assertTrue(rta)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- venv/datos.py
+++ venv/datos.py
@@ -1,34 +1,30 @@
-
-
 informacion = {
     "nasa": {
         "url": "https://api.nasa.gov/planetary/apod?api_key=d3f8ZbiJGz1p8AXp3NhZRtlJXq1RRBmiHzST8Dc2",
+        "params": {},
+        "headers": {},
         "llaves": ('copyright', 'date', 'explanation', 'hdurl', 'media_type', 'service_version', 'title', 'url')
     },
     "covid":{
         "url": "https://covid-19-data.p.rapidapi.com/country",
-        "country": "querystring = {'name': 'argentina'}",
-        "headers": "{'x-rapidapi-key': '5aafbd6efbmsh384cffe4fd6bdb6p11f423jsn23152897beea','x-rapidapi-host': 'covid-19-data.p.rapidapi.com'}",
-        "funcion": "requests.request('GET', url, headers=headers, params=querystring)"
+        "params": {'name': 'argentina'},
+        "headers": {'x-rapidapi-key': '5aafbd6efbmsh384cffe4fd6bdb6p11f423jsn23152897beea','x-rapidapi-host': 'covid-19-data.p.rapidapi.com'},
+        "funcion": "requests.get(url, headers=headers, params=params)",
+        "llaves": ('country', 'code', 'confirmed', 'recovered', 'critical', 'deaths', 'latitude', 'longitude', 'lastChange', 'lastUpdate')
         },
     "finanzas":{
-        "url": "https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote",
-        "querystring": {"symbols": "AAPL"},
-        "headers": "{'x-rapidapi-key': '5aafbd6efbmsh384cffe4fd6bdb6p11f423jsn23152897beea',    'x-rapidapi-host': 'yahoo-finance-low-latency.p.rapidapi.com'}",
-        "funcion": "requests.request('GET', url, headers=headers, params=querystring)"
+        "url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes",
+        "params": {"region":"US","symbols":"AMZN"},
+        "headers": {
+            'x-rapidapi-key': "5aafbd6efbmsh384cffe4fd6bdb6p11f423jsn23152897beea",
+            'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"},
+        "funcion": "requests.get(url, headers=headers, params=params)",
+"llaves": ('country', 'code', 'confirmed', 'recovered', 'critical', 'deaths', 'latitude', 'longitude', 'lastChange', 'lastUpdate')
+    },
+    "biblia":{
+        "url": "",
+        "key": "86b4188a3755bd60f695e3cfe27fc670",
+
+
     }
 }
-
-llaves_sistemas = (
-'system-id', 'system-name', 'active', 'mantainer', 'alfresco-folder', 'nexus-repository', 'redmine-project',
-'gitlab-project')
-llaves_aplicaciones = (
-'active', 'alfresco-folder', 'application-id', 'gitlab-project', 'mantainer', 'nexus-repository', 'redmine-project',
-'system-id', 'technology')
-llaves_areas = ('area-id', 'acronym', 'description', 'active', 'created_at', 'updated_at')
-llaves_instancias = (
-'Architecture', 'active', 'age', 'approved', 'comments', 'contribution', 'criticity', 'db', 'db-engine',
-'db-engine-version', 'deploy-repo', 'development-instance', 'documentation-repo', 'external-accesibility',
-'frontend-language', 'homo-instance', 'id-sgsa', 'information-access', 'instance-id', 'instance-name', 'issue-tracker',
-'language', 'maintenance', 'objective', 'os', 'os-version', 'process-assistance', 'redundancy', 'replacement', 'risk',
-'server-language', 'software-type', 'source-repo', 'sub-module', 'system-id', 'user-count')
